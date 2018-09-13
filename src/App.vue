8a890fb4988b10cb202a68085de2fcc50606b78c
sorenhartvig <soeren.hartvig@gmail.com>
2018-09-13 17:45:54 +0200

    first

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <UdbytteSkat />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import UdbytteSkat from './components/UdbytteSkat.vue';
 
 @Component({
   components: {
-    HelloWorld,
+    UdbytteSkat,
   },
 })
 export default class App extends Vue {}
@@ -22,7 +21,7 @@ export default class App extends Vue {}
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: left;
   color: #2c3e50;
   margin-top: 60px;
 }
