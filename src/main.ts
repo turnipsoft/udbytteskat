f84b4da67c69a583c1b14adc2881b953b7ee8fb6
sorenhartvig <soeren.hartvig@gmail.com>
2018-09-14 14:49:36 +0200

    first take

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,10 @@ import '@babel/polyfill';
 import Vue from 'vue';
 import './plugins/bootstrap-vue';
 import App from './App.vue';
+//Font awesome
+import 'vue-awesome/icons'
+import Icon from 'vue-awesome/components/Icon.vue'
+Vue.component('v-icon', Icon)
 
 Vue.config.productionTip = false;
 
